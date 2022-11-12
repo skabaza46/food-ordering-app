324d589907952b2544fd0f138d4996772891feb3
skabaza <skabaza46@gmail.com>
2022-11-12 17:33:54 -0500

    updated list of dummy food data.

--- src/components/Meals/dummy-meals.js
+++ src/components/Meals/dummy-meals.js
@@ -1,25 +1,49 @@
 
-const DUMMY_MEALS = [
+export const DUMMY_MEALS = [
   {
     id: 'm1',
+    name: 'Goat Pepper Soup',
+    description: 'A Burundi specialty',
+    price: 25.40
+  },
+  {
+    id: 'm2',
+    name: 'Cassava Leaf Soup',
+    description: 'A Congo specialty',
+    price: 18.99
+  },
+  {
+    id: 'm3',
+    name: 'Fufu with Okra Soup',
+    description: 'Burundi and Rwanda specialty',
+    price: 30.00
+  },
+  {
+    id: 'm4',
+    name: 'Ackee and codfish',
+    description: "Jamaican' most famous food.",
+    price: 17.50
+  },
+  {
+    id: 'm5',
     name: 'Sushi',
     description: 'Finest fish and veggies',
     price: 22.99,
   },
   {
-    id: 'm2',
+    id: 'm6',
     name: 'Schnitzel',
     description: 'A german specialty!',
     price: 16.5,
   },
   {
-    id: 'm3',
+    id: 'm7',
     name: 'Barbecue Burger',
     description: 'American, raw, meaty',
     price: 12.99,
   },
   {
-    id: 'm4',
+    id: 'm8',
     name: 'Green Bowl',
     description: 'Healthy...and green...',
     price: 18.99,
