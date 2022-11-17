37fba73993c2926b7bbbe4f31daf119103c0e6cc
skabaza <skabaza46@gmail.com>
2022-11-17 11:28:34 -0500

    Getting data from firebase using http and added error handling while fetching data.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,15 +1,78 @@
+import { useEffect, useState } from 'react';
+
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 
 import classes from './AvailableMeals.module.css';
 
-import {DUMMY_MEALS} from './dummy-meals';
+// import {DUMMY_MEALS} from './dummy-meals';
 
+
+const dbBaseUrl = "https://food-ordering-app-9d663-default-rtdb.firebaseio.com/foods.json";
 
 
 const AvailableMeals = () => {
+  const [ mealsList, setMealsList ] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(true);
+  const [ httpError, setHttpError ] = useState(null);
 
-  const mealsList = DUMMY_MEALS.map( (meal) => {
+  useEffect(() => {
+
+      const fetchMeals = async () => {
+        const response = await fetch(dbBaseUrl)
+
+        if (!response.ok){
+          throw new Error("Something went wrong!");
+        }
+
+        const responseData = await response.json();
+
+        const loadedMeals = [];
+
+        for (const key in responseData){
+
+            console.log(key)
+            loadedMeals.push({
+              id: key,
+              name:responseData[key].name,
+              description: responseData[key].description,
+              price: responseData[key].price
+            })
+
+          };
+
+        setMealsList(loadedMeals);
+
+        setIsLoading(false);
+      }
+
+      fetchMeals()
+      .then()
+      .catch((error)=>{
+        setIsLoading(false)
+        setHttpError(error.message)
+      });
+
+    }, []);
+
+  if (isLoading){
+    return (
+      <section className={classes.MealsLoading}>
+      <p> Loading... </p>
+      </section>
+    )
+  };
+
+
+  if(httpError){
+    return (
+      <section className={classes.MealsError}>
+      <p>{httpError}</p>
+      </section>
+    );
+  };
+
+  const mealsListItems = mealsList.map((meal) => {
 
     return (
       <MealItem
@@ -18,14 +81,15 @@ const AvailableMeals = () => {
         name={meal.name}
         description={meal.description}
         price={meal.price}
-      />);
-  });
+      />
+    );
+  })
 
   return (
     <section className={classes.meals}>
       <Card>
         <ul>
-          {mealsList}
+          {mealsListItems}
         </ul>
       </Card>
     </section>
