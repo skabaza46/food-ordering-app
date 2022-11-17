f41fb2b6bfbdcd2ef2cd2389bf40c341fcfc5fd9
skabaza <skabaza46@gmail.com>
2022-11-17 12:57:37 -0500

    Added ability to checkout the cart items.

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -69,6 +69,10 @@ const cartReducer = (state, action) => {
     }
   }
 
+  if (action.type === "CLEAR"){
+    return defaultCartState;
+  }
+
   return defaultCartState;
 };
 
@@ -86,11 +90,18 @@ const CartProvider = (props) => {
     dispatchCartAction({type: 'REMOVE', id: id});
   };
 
+  const clearCartHandler = () => {
+
+    dispatchCartAction({type: "CLEAR"});
+  };
+
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   }
   return (
     <CartContext.Provider value={cartContext}>
